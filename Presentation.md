b9f71367763e7994938bebf828fc8338265c4238
Scott Jasperse <scott.jasperse@meijer.com>
2024-12-16 11:16:39 -0500

    example-1 appears to be working

---
+++ Presentation.md
@@ -0,0 +1,17 @@
+---
+marp: true
+---
+
+
+# Example 1
+
+
+```sh
+> terraform init
+> terraform apply -var-file="../../env.tfvars"
+> dotnet new web --name WebApp --framework net8.0
+```
+
+---
+
+# Slide 2
---
+++ example-1/infrastructure/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.14.0"
+    }
+  }
+}
+
+variable "resource_prefix" {
+    type = string
+}
+
+variable "subscription_id" {
+    type = string
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+# From https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_web_app
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-1"
+  location = "East US 2"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "${var.resource_prefix}-app-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "${var.resource_prefix}-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "8.0" 
+    }
+  }
+}
